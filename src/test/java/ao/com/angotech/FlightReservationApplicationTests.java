d4d9765d472dc68c564628c433a02cf646da8179
Fernando Angolar <fernandoangolar581@gmail.com>
2025-01-16 17:02:55 +0100

    first commit

---
+++ src/test/java/ao/com/angotech/FlightReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.com.angotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
