638dbf594b0b84491fe953c2d709ced31cddb169
Fernando Angolar <fernandoangolar581@gmail.com>
2025-01-21 14:57:10 +0100

    :lipstick: feat: adicionao o meu to que verifica a existencia de um flightNUmber

--- src/main/java/ao/com/angotech/repository/FlightsRepository.java
+++ src/main/java/ao/com/angotech/repository/FlightsRepository.java
@@ -3,6 +3,10 @@ package ao.com.angotech.repository;
 import ao.com.angotech.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface FlightsRepository extends JpaRepository<Flight, Long> {
+
+    Optional<Flight> findByFlightNumber(String flightNumber);
 }
