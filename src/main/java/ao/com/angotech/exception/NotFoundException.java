3b013a77b9bc0447f7d140792a8de3adcd18c603
Fernando Angolar <fernandoangolar581@gmail.com>
2025-01-21 14:53:55 +0100

    :lipstick: feat: Classe de exception not found, lança uma exception quando a entity não existe

--- src/main/java/ao/com/angotech/exception/NotFoundException.java
+++ src/main/java/ao/com/angotech/exception/NotFoundException.java
@@ -1,2 +1,8 @@
-package ao.com.angotech.exception;public class NotFoundException {
+package ao.com.angotech.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(String msg) {
+        super(msg);
+    }
 }
