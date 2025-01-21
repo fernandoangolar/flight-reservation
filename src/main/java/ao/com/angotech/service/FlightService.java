2aeacb6174f98290fbfa59aacc3f2d0d9cb8b367
Fernando Angolar <fernandoangolar581@gmail.com>
2025-01-21 14:59:59 +0100

    :lipstick: feat: TOda regra de negocio adicionado

--- src/main/java/ao/com/angotech/service/FlightService.java
+++ src/main/java/ao/com/angotech/service/FlightService.java
@@ -1,2 +1,15 @@
-package ao.com.angotech.service;public interface FlightService {
+package ao.com.angotech.service;
+
+import ao.com.angotech.model.Flight;
+
+import java.util.List;
+
+public interface FlightService {
+
+    List<Flight> findAll();
+    Flight findById(Long id);
+    Flight create(Flight flight);
+    Flight update(Long id, Flight flight);
+
+    void delete(Long id);
 }
