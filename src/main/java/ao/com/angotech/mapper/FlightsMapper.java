067dc371adbfed4f7b0b9b812439b45b51752bd9
Fernando Angolar <fernandoangolar581@gmail.com>
2025-01-22 05:36:58 +0100

    :lipstick: feat: Classe Mapper criada, para converter um dto para uma entity

---
+++ src/main/java/ao/com/angotech/mapper/FlightsMapper.java
@@ -0,0 +1,25 @@
+package ao.com.angotech.mapper;
+
+import ao.com.angotech.dto.FlightsDto;
+import ao.com.angotech.model.Flight;
+
+public class FlightsMapper {
+
+    public static Flight fromDtoToEntity(FlightsDto dto) {
+
+        Flight flight = new Flight();
+        flight.setId(dto.getId());
+        flight.setFlightNumber(dto.getFlightNumber());
+        flight.setAirline(dto.getAirline());
+        flight.setArrivalTime(dto.getArrivalTime());
+        flight.setDestination(flight.getDestination());
+        flight.setAvailableSeats(dto.getAvailableSeats());
+        flight.setCreated_at(dto.getCreated_at());
+        flight.setDepartureTime(dto.getDepartureTime());
+        flight.setOrigin(dto.getOrigin());
+        flight.setPrice(dto.getPrice());
+        flight.setUpdated_at(dto.getUpdated_at());
+
+        return flight;
+    }
+}
