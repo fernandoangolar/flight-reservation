fd89a57e36375b589d5f58d75e990cdef02e9ec8
Fernando Angolar <fernandoangolar581@gmail.com>
2025-01-22 05:35:57 +0100

    :lipstick: feat: Classe dto criada

---
+++ src/main/java/ao/com/angotech/dto/FlightsDto.java
@@ -0,0 +1,139 @@
+package ao.com.angotech.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+public class FlightsDto {
+
+    private Long id;
+
+    private String airline;
+
+    private String flightNumber;
+
+    private String origin;
+
+    private String destination;
+
+    private Timestamp departureTime;
+
+    private Integer availableSeats;
+
+    private Timestamp arrivalTime;
+
+    private BigDecimal price;
+
+    private Timestamp created_at;
+
+    private Timestamp updated_at;
+
+    public FlightsDto() {}
+
+    public FlightsDto(Long id, String airline, String flightNumber, String origin, String destination, Timestamp departureTime, Integer availableSeats, Timestamp arrivalTime, BigDecimal price, Timestamp created_at, Timestamp updated_at) {
+        this.id = id;
+        this.airline = airline;
+        this.flightNumber = flightNumber;
+        this.origin = origin;
+        this.destination = destination;
+        this.departureTime = departureTime;
+        this.availableSeats = availableSeats;
+        this.arrivalTime = arrivalTime;
+        this.price = price;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAirline() {
+        return airline;
+    }
+
+    public void setAirline(String airline) {
+        this.airline = airline;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Timestamp getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Timestamp departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Integer getAvailableSeats() {
+        return availableSeats;
+    }
+
+    public void setAvailableSeats(Integer availableSeats) {
+        this.availableSeats = availableSeats;
+    }
+
+    public Timestamp getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(Timestamp arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+}
