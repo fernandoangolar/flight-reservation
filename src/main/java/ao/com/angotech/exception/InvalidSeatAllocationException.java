aff068c4d13a125ce3f9925ec15222f7b307cfbf
Fernando Angolar <fernandoangolar581@gmail.com>
2025-01-24 11:13:37 +0100

    :lipstick: feat: exception para vãlidar os acentos das flights

---
+++ src/main/java/ao/com/angotech/exception/InvalidSeatAllocationException.java
@@ -0,0 +1,8 @@
+package ao.com.angotech.exception;
+
+public class InvalidSeatAllocationException extends RuntimeException {
+
+    public InvalidSeatAllocationException(String msg) {
+        super(msg);
+    }
+}
