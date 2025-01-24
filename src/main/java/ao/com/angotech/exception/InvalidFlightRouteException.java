d10dde77590babfe166485bebd44bc3bfb20acd8
Fernando Angolar <fernandoangolar581@gmail.com>
2025-01-24 11:11:48 +0100

    :lipstick: feat: exception para válidar as rotas das flights

---
+++ src/main/java/ao/com/angotech/exception/InvalidFlightRouteException.java
@@ -0,0 +1,8 @@
+package ao.com.angotech.exception;
+
+public class InvalidFlightRouteException extends RuntimeException {
+
+    public InvalidFlightRouteException(String msg) {
+        super(msg);
+    }
+}
