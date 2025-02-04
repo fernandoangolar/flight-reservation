f6072a9e6051cdc91060a1762e86561317d6572a
Fernando Angolar <fernandoangolar581@gmail.com>
2025-02-04 12:24:00 +0100

    :lipstick: feat: Enum de role criada

---
+++ src/main/java/ao/com/angotech/model/User.java
@@ -0,0 +1,116 @@
+package ao.com.angotech.model;
+
+
+import ao.com.angotech.enums.RoleEnum;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    private String password;
+
+    private RoleEnum role;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(updatable = false)
+    @CreationTimestamp
+    @JsonProperty("created_at")
+    private Timestamp created_at;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @UpdateTimestamp
+    @JsonProperty("updated_at")
+    private Timestamp updated_at;
+
+    public User() {
+    }
+
+    public User(Long id, String firstName, String lastName, String email, String password, RoleEnum role, Timestamp created_at, Timestamp updated_at) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public RoleEnum getRole() {
+        return role;
+    }
+
+    public void setRole(RoleEnum role) {
+        this.role = role;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+}
