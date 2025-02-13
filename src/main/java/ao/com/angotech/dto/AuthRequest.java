a9e285ebe47521a4bdca1adfd29604c9b9968a50
Fernando Angolar <fernandoangolar581@gmail.com>
2025-02-13 05:35:24 +0100

    :lipstick: feat(auth): adicionar DTO AuthRequest para requisições de login

---
+++ src/main/java/ao/com/angotech/dto/AuthRequest.java
@@ -0,0 +1,78 @@
+package ao.com.angotech.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class AuthRequest {
+
+    @NotBlank(message = "O primeiro nome é obrigatório")
+    private String firstName;
+
+    @NotBlank(message = "O último nome é obrigatório")
+    private String lastName;
+
+    @Email(message = "E-mail inválido")
+    @NotBlank(message = "O e-mail é obrigatório")
+    private String email;
+
+    @NotBlank(message = "O telefone é obrigatório")
+    @Size(min = 9, max = 15, message = "O telefone deve ter entre 9 e 15 caracteres")
+    private String phone;
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 6, message = "A senha deve ter pelo menos 6 caracteres")
+    private String password;
+
+    public AuthRequest() {
+    }
+
+    public AuthRequest(String firstName, String lastName, String email, String phone, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
